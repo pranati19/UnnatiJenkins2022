f9746333b447ecb215cf67b54a9c6d74ebe04cff
pranati19 <107803766+pranati19@users.noreply.github.com>
2022-06-20 12:30:23 +0100

    Update

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-print("Hello all from my Python First project")
-print("Hello all from my Python First project")
+print("Hello all from my PRADEEP First project")
+print("Hello all from my PRADEEP First project")
 print("Hello all from my Python First project")
 print("Hello all from my Python First project")
